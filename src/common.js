fda61a6e34b37109276a4f8d0ebaf49f5f729fa2
Matt Bell <mappum@gmail.com>
2017-11-17 21:27:12 -0800

    Added some type inference in tx normalizer

--- src/common.js
+++ src/common.js
@@ -19,9 +19,39 @@ let burnHandler = {
   onOutput () {}
 }
 
+// to make basic transactions easier to compose, we let users
+// specify them in a simpler format and then expand them here
+// to the full format
 function normalizeTx (tx) {
+  // user can specify single values for to/from,
+  // wrap them in arrays
   if (!Array.isArray(tx.from)) tx.from = [ tx.from ]
   if (!Array.isArray(tx.to)) tx.to = [ tx.to ]
+
+  // if no type, infer that type is 'accounts'
+  // (convenience for built-in coins handlers)
+  for (let input of tx.from) {
+    if (input.type != null) continue
+    input.type = 'accounts'
+
+    // infer accountType (either pubkey or multisig)
+    if (input.accountType != null) continue
+    input.accountType = input.pubkey ? 'pubkey' : 'multisig'
+  }
+
+  // if output has address and no type, infer that type is 'accounts'
+  for (let output of tx.to) {
+    if (output.type != null) continue
+    if (output.address == null) continue
+    output.type = 'accounts'
+  }
+
+  // if there is only 1 input and it has no amount, set amount
+  // to sum of outputs
+  if (tx.from.length === 1 && tx.from[0].amount == null) {
+    let amountOut = tx.to.reduce((sum, { amount }) => sum + amount, 0)
+    tx.from[0].amount = amountOut
+  }
 }
 
 function clone (obj) {
