62c982682ea357495a01876ba804f4803f287871
Matt Bell <mappum@gmail.com>
2018-04-07 18:42:00 -0700

    Made Wallet more classy

--- src/wallet.js
+++ src/wallet.js
@@ -1,43 +1,121 @@
-let secp = require('secp256k1')
-let coins = require('../index')
 let { randomBytes } = require('crypto')
+let fs = require('fs')
+let { join } = require('path')
+let mkdirp = require('mkdirp').sync
+let secp = require('secp256k1')
+let home = require('user-home')
+let coins = require('..')
 
-module.exports = function(priv, client) {
-  if (!Buffer.isBuffer(priv) || priv.length !== 32) {
-    throw Error('Private key must be a 32-byte buffer')
+// TODO: HD scheme for deriving per-GCI keys
+// TODO: key encryption
+// TODO: support user-provided seed
+
+class Wallet {
+  constructor (privkey, client) {
+    if (!Buffer.isBuffer(privkey) || privkey.length !== 32) {
+      throw Error('"privkey" must be a 32-byte Buffer')
+    }
+
+    this.client = client
+    this.privkey = privkey
+    this.pubkey = secp.publicKeyCreate(this.privkey)
+    this._address = getAddress(this.pubkey)
   }
 
-  let creds = {}
-  creds.priv = priv
-  creds.pub = secp.publicKeyCreate(creds.priv)
-  creds.address = coins.secp256k1Account.getAddress({ pubkey: creds.pub })
+  address () {
+    return this._address
+  }
 
-  return {
-    address: creds.address,
-    priv: creds.priv,
-    pub: creds.pub,
-    getBalance: async function() {
-      let account = await client.state.accounts[creds.address]
-      if (account) {
-        return account.balance
-      } else {
-        return 0
-      }
-    },
-    send: async function(address, amount) {
-      let account = await client.state.accounts[creds.address]
-      let tx = {
-        from: {
-          amount: amount,
-          sequence: account ? account.sequence : 0,
-          pubkey: creds.pub
-        },
-        to: { amount, address }
-      }
+  async balance () {
+    this.assertClient()
+    let accounts = this.client.state.accounts
+    let account = await accounts[this._address]
+    if (account == null) {
+      return 0
+    }
+    return account.balance
+  }
 
-      let sigHash = coins.getSigHash(tx)
-      tx.from.signature = secp.sign(sigHash, creds.priv).signature
-      return await client.send(tx)
+  async send (toAddress, amount) {
+    this.assertClient()
+    if (typeof toAddress !== 'string') {
+      throw Error('"toAddress" must be a string')
+    }
+    if (!Number.isInteger(amount)) {
+      throw Error('"amount" must be an integer')
+    }
+
+    // get our account sequence number
+    let accounts = this.client.state.accounts
+    let account = await accounts[this._address]
+
+    // build tx
+    let tx = {
+      from: {
+        amount,
+        sequence: account ? account.sequence : 0,
+        pubkey: this.pubkey
+      },
+      to: {
+        amount,
+        address: toAddress
+      }
+    }
+
+    // sign tx
+    let sigHash = coins.getSigHash(tx)
+    tx.from.signature = secp.sign(sigHash, this.privkey).signature
+
+    // broadcast tx
+    return this.client.send(tx)
+  }
+
+  assertClient () {
+    if (this.client == null) {
+      throw Error('Not connected with a Lotion light client')
     }
   }
 }
+
+function getAddress (pubkey) {
+  return coins.secp256k1Account.getAddress({ pubkey })
+}
+
+function defaultPath () {
+  return join(home, '.coins')
+}
+
+function createOrLoadSeed (path) {
+  let seedPath = join(path, 'secret')
+
+  let exists
+  try {
+    fs.accessSync(seedPath)
+    exists = true
+  } catch (err) {
+    exists = false
+  }
+
+  // load existing seed
+  if (exists) {
+    return Buffer.from(fs.readFileSync(seedPath), 'hex')
+  }
+
+  // generate random seed
+  let seed = randomBytes(32)
+  mkdirp(path)
+  fs.writeFileSync(seedPath, seed, 'hex')
+  return seed
+}
+
+function createOrLoadWallet (path = defaultPath(), client) {
+  if (typeof path === 'object') {
+    client = path
+    path = defaultPath()
+  }
+
+  let privkey = createOrLoadSeed(path)
+  return new Wallet(privkey, client)
+}
+
+module.exports = createOrLoadWallet
