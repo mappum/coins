c8ee163789d3bd07b170bd36dafc204c7ea50950
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-04-11 09:04:26 -0400

    fix npx coins command

--- bin.js
+++ bin.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 
+let fs = require('fs')
+let os = require('os')
+const { randomBytes } = require('crypto')
+
 let { wallet } = require('.')
 
 // generates or loads wallet from default path (~/.coins)
-let address = wallet().address()
+
+let path = os.homedir() + '/.coins'
+!fs.existsSync(path) && fs.writeFileSync(path, randomBytes(32))
+let privkey = fs.readFileSync(path)
+
+let address = wallet(privkey).address()
 
 console.error('Your Address:')
 console.log(address)
