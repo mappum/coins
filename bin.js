ee0da14c908c6c124932970d87dc33e84c46ed60
Matt Bell <mappum@gmail.com>
2018-04-16 09:23:14 -0700

    Fixed style

--- bin.js
+++ bin.js
@@ -2,17 +2,17 @@
 
 let fs = require('fs')
 let os = require('os')
-const secp256k1 = require('secp256k1')
-const { randomBytes } = require('crypto')
-
+let secp256k1 = require('secp256k1')
+let { randomBytes } = require('crypto')
 let { wallet } = require('.')
 
 // generates or loads wallet from default path (~/.coins)
-
 let path = os.homedir() + '/.coins'
 let privKey
 if (!fs.existsSync(path)) {
-  do {privKey = randomBytes(32)} while (!secp256k1.privateKeyVerify(privKey))
+  do {
+    privKey = randomBytes(32)
+  } while (!secp256k1.privateKeyVerify(privKey))
   fs.writeFileSync(path, privKey.toString('hex'))
 } else {
   privKey = Buffer.from(fs.readFileSync(path, 'utf8'), 'hex')
